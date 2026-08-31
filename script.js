834324ad02e9b91a9c68f26ae7aa08d500fca795
sabbirrayhan986-ui <sabbirrayhan986@gmail.com>
2026-08-31 16:28:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showEvaluationMessage() {
 
+    alert(
+        "Teaching Evaluation Demo\n\n" +
+        "This is a demonstration page. " +
+        "The Teaching Evaluation feature is not connected to any real university system."
+    );
+
+}
